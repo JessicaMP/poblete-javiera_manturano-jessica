0916f571de411dd4265c903f71e95da988d81db9
Jessica <jessica95.mp@gmail.com>
2024-06-23 12:59:32 -0500

    implementacion login

---
+++ ClinicaOdontologica/src/main/java/BackEndC3/ClinicaOdontologica/controller/OdontologoController.java
@@ -0,0 +1,57 @@
+package BackEndC3.ClinicaOdontologica.controller;
+
+import BackEndC3.ClinicaOdontologica.entity.Odontologo;
+import BackEndC3.ClinicaOdontologica.exception.ResourceNotFoundException;
+import BackEndC3.ClinicaOdontologica.service.OdontologoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/odontologos")
+public class OdontologoController {
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @PostMapping
+    public ResponseEntity<Odontologo> guardarOdontologo(@RequestBody Odontologo odontologo) {
+        return ResponseEntity.ok(odontologoService.registrarOdontologo(odontologo));
+    }
+
+    @PutMapping
+    public ResponseEntity<String> actualizarOdontologo(@RequestBody Odontologo odontologo) throws ResourceNotFoundException {
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorId(odontologo.getId());
+        if (odontologoBuscado != null) {
+            odontologoService.actualizarOdontologo(odontologo);
+            return ResponseEntity.ok("Odontologo " + odontologo.getId() + " actualizado con éxito");
+        }
+        throw new ResourceNotFoundException("Odontologo no encontrado");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Odontologo>> listarTodos() {
+        return ResponseEntity.ok(odontologoService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Odontologo>> getOdontologo(@PathVariable Long id) throws ResourceNotFoundException {
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorId(id);
+        if (odontologoBuscado.isPresent()) {
+            return ResponseEntity.ok(odontologoBuscado);
+        }
+        throw new ResourceNotFoundException("Odontologo no encontrado");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> eliminarOdontologo(Long id) throws ResourceNotFoundException {
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorId(id);
+        if (odontologoBuscado.isPresent()) {
+            odontologoService.eliminarOdontologo(id);
+            return ResponseEntity.ok("Odontologo eliminado con exito");
+        }
+        throw new ResourceNotFoundException("Odontologo no encontrado");
+    }
+}
